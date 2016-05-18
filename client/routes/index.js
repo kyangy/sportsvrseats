7068a3fae2a6ffe5abe4f238f199e866efda64bf
Kevin Yang <kyang012@ucr.edu>
2016-05-18 14:21:14 -0700

    Created about and product routes

--- client/routes/index.js
+++ client/routes/index.js
@@ -3,9 +3,13 @@ import { Route, IndexRoute } from 'react-router'
 
 import App from '../container'
 import Home from '../components/Home'
+import About from '../components/about'
+import Product from '../components/product'
 
 export default (
   <Route name="app" component={ App } path='/' >
     <IndexRoute component={ Home } />
+    <Route component={ About } path='about' />
+    <Route component={ Product } path='product' />
   </Route>
 )
