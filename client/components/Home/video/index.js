1648cdef69dd9713c3f40b800a22c0571f5f7805
Kevin Yang <kyang012@ucr.edu>
2016-05-24 11:38:38 -0700

    Updated styling of video component

--- client/components/Home/video/index.js
+++ client/components/Home/video/index.js
@@ -1,13 +1,31 @@
 'use strict'
 
 import React, { Component, PropTypes } from 'react'
+import { Link } from 'react-router'
+
+require('./index.scss')
 
 export default class extends Component {
   render() {
     return (
-      <video autoPlay loop muted>
-        <source src={require('../../../assets/videos/courtside.mp4')} type='video/mp4' />
-      </video>
+      <div className="video-banner">
+        <div className="video-frame">
+          <video autoPlay loop muted>
+            <source src={require('../../../assets/videos/courtside.mp4')} type='video/mp4' />
+          </video>
+        </div>
+        <div className="overlay">
+          <div className="text-box">
+            <div className="text">
+              <p>SportsVRSeats</p>
+            </div>
+            <div>
+              <Link to="sports"><i className="fa fa-ticket" aria-hidden="true"></i></Link>
+              <Link to="about"><i className="fa fa-video-camera" aria-hidden="true"></i></Link>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
