0a6effaaaec27d055ec24ee85b01c0689be264d6
Kevin Yang <kyang012@ucr.edu>
2016-05-25 16:57:20 -0700

    Updated config, package, prod webpack

--- webpack/index.js
+++ webpack/index.js
@@ -6,9 +6,5 @@ const prodConfig = require('./prod.config')
 exports.devConfig = devConfig
 exports.prodConfig = prodConfig
 
-if (__DEVELOPMENT__) {
-  exports.compiler = require('webpack')(devConfig)
-}
-
 exports.compiler = require('webpack')(prodConfig)
 
--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,28 +1,51 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const StatsPlugin = require('stats-webpack-plugin')
+const webpack = require('webpack')
+const config = require('../config')
 
 module.exports = {
   devtool: 'source-map',
+  context: path.resolve(__dirname, '..'),
   entry: [
     path.resolve(__dirname, '../client/index')
   ],
+  resolve: {
+    modulesDirectories: ['node_modules'],
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
-    path: path.join(__dirname, '../static/dist'),
+    path: path.join(__dirname, '../static/dist/'),
     filename: 'bundle.min.js',
     publicPath: '/dist/'
   },
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
+    new ExtractTextPlugin('bundle.min.css', {
+      allChunks: true
     }),
+    new webpack.DefinePlugin({
+      __CLIENT__: true,
+      __SERVER__: false,
+      __DEVELOPMENT__: false,
+      __DEVTOOLS__: false
+    }),
+    // ignore dev config
+    new webpack.IgnorePlugin(/\.\/dev/, /\/config$/),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
-        warnings: false
+        warnings: false,
+        screw_ie8: true
       }
-    })
+    }),
+    new StatsPlugin('webpack.stats.json', {
+      source: false,
+      modules: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
   ],
   module: {
     loaders: [
