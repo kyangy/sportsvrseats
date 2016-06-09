80b2e8ab566948264223472a345a816d6f289b64
Kevin Yang <kyang012@ucr.edu>
2016-06-08 17:50:12 -0700

    Updated signup component

--- shared/components/signup/index.js
+++ shared/components/signup/index.js
@@ -1,17 +1,45 @@
 'use strict'
 
 import React, { Component, PropTypes } from 'react'
+import * as Actions from '../../redux/actions/users'
+import { connect } from 'react-redux'
 
 import SignUpForm from './signup-form'
 
-export default class SignUp extends Component {
+class SignUp extends Component {
+  static propTypes = {
+    users: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired,
+      password: PropTypes.string.isRequired,
+    })).isRequired,
+    dispatch: PropTypes.func.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.add = this.add.bind(this);
+  }
+
+  add(name, email, password) {
+    this.props.dispatch(Actions.addUserRequest({ name, email, password }));
+  }
+
   render() {
     return (
       <div>
         <div className="container">
-          <SignUpForm />
+          <SignUpForm addUser={this.add} />
         </div>
       </div>
     )
   }
 }
+
+function mapStateToProps(store) {
+  return {
+    users: store.users,
+  };
+}
+
+export default connect(mapStateToProps)(SignUp);
--- shared/components/signup/signup-form/index.js
+++ shared/components/signup/signup-form/index.js
@@ -2,15 +2,35 @@
 
 import React, { Component, PropTypes } from 'react'
 
-export default class SignUpForm extends Component {
+class SignUpForm extends Component {
+  static propTypes = {
+    addUser: PropTypes.func.isRequired
+  };
+
+  constructor(props) {
+    super(props);
+    this.addUser = this.addUser.bind(this);
+  }
+
+  addUser() {
+    const userName = this.refs.name;
+    const userEmail = this.refs.email;
+    const userPassword = this.refs.password;
+    if (userName.value && userEmail.value && userPassword.value) {
+      this.props.addUser(userName.value, userEmail.value, userPassword.value);
+    }
+  }
+
   render() {
     return (
       <div>
-        <input type="text" placeholder="Name"/>
-        <input type="text" placeholder="Email"/>
-        <input type="password" placeholder="Password"/>
-        <button>Sign Up!</button>
+        <input type="text" placeholder="Name" ref="name"/>
+        <input type="text" placeholder="Email" ref="email"/>
+        <input type="password" placeholder="Password" ref="password"/>
+        <button href="#" onClick={this.addUser}>Sign Up!</button>
       </div>
     )
   }
 }
+
+export default SignUpForm;
